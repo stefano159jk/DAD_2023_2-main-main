dd4cbfac93cd40a5b149d35b5d510c7cd0b87df7
denilson583 <denilson.mamani@upeu.edu.pe>
2024-05-20 09:31:35 -0500

    asdasasdsad

---
+++ ms-gateway-server/src/main/java/com/example/msgatewayserver/dto/TokenDto.java
@@ -0,0 +1,17 @@
+package com.example.msgatewayserver.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+
+
+@AllArgsConstructor
+@Data
+public class TokenDto {
+    private String token;
+}
