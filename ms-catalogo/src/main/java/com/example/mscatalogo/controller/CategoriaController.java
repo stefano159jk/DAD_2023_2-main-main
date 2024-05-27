68d97abfced97c3d06368ea4edc1572205f4d565
denilson583 <denilson.mamani@upeu.edu.pe>
2024-05-27 11:28:50 -0500

    asasdsdasda

--- ms-catalogo/src/main/java/com/example/mscatalogo/controller/CategoriaController.java
+++ ms-catalogo/src/main/java/com/example/mscatalogo/controller/CategoriaController.java
@@ -2,10 +2,17 @@ package com.example.mscatalogo.controller;
 
 import com.example.mscatalogo.entity.Categoria;
 import com.example.mscatalogo.service.CategoriaService;
+import com.example.mscatalogo.utils.PdfUtils;
+import com.itextpdf.text.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -38,6 +45,16 @@ public class CategoriaController {
     public ResponseEntity<List<Categoria>>eliminar(@PathVariable(required = true) Integer id){
         categoriaService.eliminar(id);
         return ResponseEntity.ok(categoriaService.listar());
+    }
+    @GetMapping("/pdf")
+    public ResponseEntity<byte[]> exportPdf() throws IOException, DocumentException {
+        //List<Map<String, Object>> queryResults = myService.executeQuery(request);
+        ByteArrayOutputStream pdfStream = PdfUtils.generatePdfStream();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=query_results.pdf");
+        headers.setContentLength(pdfStream.size());
+        return new ResponseEntity<>(pdfStream.toByteArray(), headers, HttpStatus.OK);
     }
 
 }
